79c11ef1ba4c8e7ab8c76a0daa7c585835a4d819
SebastianG <sebastiangimenezomar@gmail.com>
2022-10-06 17:47:46 -0300

    subiendo repositorio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='contenedor-principal'>
+      <h1>Prueba de componentes de react</h1>
+      <Testimonio 
+        nombre="pedro"
+        pais="argentina"
+        imagen="1"
+        cargo="freelancer"
+        testimonio="commodo aliqua exercitation sunt ea et fugiat lorem dolor in commodo proident et minim culpa occaecat cillum cillum nostrud. aliquip occaecat ut sed veniam labore proident laboris qui ad do exercitation dolore pariatur pariatur est laboris officia. eiusmod laboris nostrud nostrud ex quis culpa pariatur officia lorem enim ipsum irure id lorem laboris. Excepteur mollit sint enim lorem elit cillum labore est consequat non mollit est consequat anim eu consectetur. ipsum non adipiscing irure ut qui deserunt ipsum magna in ullamco nulla exercitation pariatur proident pariatur lorem ex officia. in est irure reprehenderit amet laboris eiusmod nostrud nostrud in anim exercitation qui cillum sed cillum exercitation."/>
+
+      <Testimonio 
+        nombre="gonzalo"
+        pais="argentina"
+        imagen="2"
+        cargo="freelancer"
+        testimonio="commodo aliqua exercitation sunt ea et fugiat lorem dolor in commodo proident et minim culpa occaecat cillum cillum nostrud. aliquip occaecat ut sed veniam labore proident laboris qui ad do exercitation dolore pariatur pariatur est laboris officia. eiusmod laboris nostrud nostrud ex quis culpa pariatur officia lorem enim ipsum irure id lorem laboris. Excepteur mollit sint enim lorem elit cillum labore est consequat non mollit est consequat anim eu consectetur. ipsum non adipiscing irure ut qui deserunt ipsum magna in ullamco nulla exercitation pariatur proident pariatur lorem ex officia. in est irure reprehenderit amet laboris eiusmod nostrud nostrud in anim exercitation qui cillum sed cillum exercitation."/>
+
+      <Testimonio 
+        nombre="rosa"
+        pais="argentina"
+        imagen="3"
+        cargo="freelancer"
+        testimonio="commodo aliqua exercitation sunt ea et fugiat lorem dolor in commodo proident et minim culpa occaecat cillum cillum nostrud. aliquip occaecat ut sed veniam labore proident laboris qui ad do exercitation dolore pariatur pariatur est laboris officia. eiusmod laboris nostrud nostrud ex quis culpa pariatur officia lorem enim ipsum irure id lorem laboris. Excepteur mollit sint enim lorem elit cillum labore est consequat non mollit est consequat anim eu consectetur. ipsum non adipiscing irure ut qui deserunt ipsum magna in ullamco nulla exercitation pariatur proident pariatur lorem ex officia. in est irure reprehenderit amet laboris eiusmod nostrud nostrud in anim exercitation qui cillum sed cillum exercitation."/>  
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,19 @@
+import React from "react";
+import '../hojas-de-estilo/Testimonio.css';
+
+function Testimonio(props) {
+  return (
+    <div className="contenedor-testimonio">
+      <img className="imagen-testimonio"
+      src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+      alt="foto 1"/>
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className="cargo-testimonio"><strong>{props.cargo}</strong></p>
+        <p className="texto-testimonio">{props.testimonio}</p>
+      </div>
+    </div>
+  );    
+}
+
+export default Testimonio; 
